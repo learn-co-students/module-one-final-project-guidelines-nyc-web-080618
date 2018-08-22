da236dc37493dc75989e94bfbd30ddffd13e44b1
Jesse McReady <jessemcready@gmail.com>
2018-08-22 09:12:32 -0400

    added links to branches and languages in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   has_many :user_repositories
   has_many :repositories, through: :user_repositories
+
+  has_many :languages, through: :repositories
+
+  has_many :branches, through: :repositories
 end
