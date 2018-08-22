83e55a5911abbf7d35fa50021e14b39210b8f10c
mguevara717 <marissa.guevara717@gmail.com>
2018-08-22 09:42:15 -0400

    added seedd file

--- bin/run.rb
+++ bin/run.rb
@@ -1,6 +1,6 @@
 require_relative '../config/environment'
 
-def greet(user_response)
-  puts "hey, what's your progress?"
-  user_response = gets.chomp
-end
+# def greet(user_response)
+#   puts "hey, what's your progress?"
+#   user_response = gets.chomp
+# end
