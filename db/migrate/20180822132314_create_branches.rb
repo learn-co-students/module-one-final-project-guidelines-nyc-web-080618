3fb613514824d656d89310cd784875f056ef3b52
Jesse McReady <jessemcready@gmail.com>
2018-08-22 09:55:19 -0400

    added seed data and changed a column name in repository languages

--- db/migrate/20180822132314_create_branches.rb
+++ db/migrate/20180822132314_create_branches.rb
@@ -2,6 +2,7 @@ class CreateBranches < ActiveRecord::Migration[5.0]
   def change
     create_table :branches do |t|
       t.string :name
+      t.integer :repository_id
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,29 @@
 require 'faker'
 
+puts "clearing all existing data"
+  User.destroy_all
+  Repository.destroy_all
+  UserRepository.destroy_all
+  Language.destroy_all
+  RepositoryLanguage.destroy_all
+  Branch.destroy_all
+puts "done clearing"
+
 puts "creating users"
-  10.times do |i|
+  5.times do |i|
     User.create(name: Faker::Name.unique.name)
   end
 puts "done with users"
 
 puts "creating repositories"
-  10.times do |i|
+  100.times do |i|
     Repository.create(name: Faker::MichaelScott.unique.quote)
   end
 puts "done with repositories"
 
 puts "creating user_repositories"
-  10.times do |i|
-    UserRepository.create(user_id: User.sample.id, repository_id: Repository.sample.id)
+  100.times do |i|
+    UserRepository.create(user_id: User.all.sample.id, repository_id: Repository.all.sample.id)
   end
 puts "done with user_repositories"
 
@@ -25,13 +34,13 @@ puts "creating languages"
 puts "done with languages"
 
 puts "creating repository_languages"
-  10.times do |i|
-    RepositoryLanguage.create(repository_id: Repository.sample.id, language_id: Language.sample.id)
+  100.times do |i|
+    RepositoryLanguage.create(repository_id: Repository.all.sample.id, language_id: Language.all.sample.id)
   end
 puts "done with repository_languages"
 
 puts "creating branches"
   10.times do |i|
-    Branch.create(name: Faker::SiliconValley.unique.company, repository_id: Repository.sample.id)
+    Branch.create(name: Faker::SiliconValley.unique.company, repository_id: Repository.all.sample.id)
   end
 puts "done with branches"
