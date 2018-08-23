00b20a42bd13b5f026815dbfd65b27e7bcebc793
Jesse McReady <jessemcready@gmail.com>
2018-08-23 10:22:30 -0400

    added columns to user, created table and model for follower, fixed seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,39 +1,40 @@
 require 'pry'
 class User < ActiveRecord::Base
-  @forked_repositories = []
+  validates :name, presence: true
+  validates :username, presence: true
+  validates :email, presence: true
 
-  attr_accessor :forked_repositories
+  validates :email, uniqueness: true
+  validates :username, uniqueness: true
 
   has_many :user_repositories
   has_many :repositories, through: :user_repositories
-  # has_many :repositories, through: :user_repositories
 
   has_many :languages, through: :repositories
 
   has_many :branches, through: :repositories
 
-  # has_many :forks, through: :repositories
-
-
+  has_many :followers, :foreign_key => :follower_id
+  has_many :followees, :class_name => "User", :foreign_key => :followee_id
 
   def create_repo(name)
     repo = Repository.create(name: name)
     UserRepository.create(user_id: self.id, repository_id: repo.id, forked: false)
-  end
+  end # end create_repo
 
   def fork_repo(repository)
     # add our user to that repo
     repo = create_repo(repository.name)
     repo.update(forked: true)
     UserRepository.create(user_id: self.id, repository_id: repository.id, forked: true)
-  end
+  end # end fork_repo
 
   def forks
     user_forks_arr = user_repositories.where(forked: true)
     user_forks_arr.map do |user_fork|
       user_fork.repository
-    end
-  end
+    end # end map
+  end # end forks
 
   def users_that_forked_our_repos
     # get all our original repos
@@ -42,23 +43,23 @@ class User < ActiveRecord::Base
     our_repos = user_repositories.where(forked: false)
     our_forked_users = our_repos.map do |repo|
       repo.repository.users
-    end.flatten
+    end.flatten # end map
     our_forked_users.select do |user|
       user.id != self.id
-    end
-  end
+    end # end select
+  end # end users_that_forked_our_repos
 
-  def self.search_repositories_by_username(name)
-    user = User.find_by(name: name)
+  def self.search_repositories_by_username(username)
+    user = User.find_by(username: username)
     user_repos = UserRepository.where(user_id: user.id)
     user_repos.map do |user_repo|
       user_repo.repository
-    end
-  end
+    end # end map
+  end # end self.search_repositories_by_username
 
   def add_collaborator(user, repository)
     UserRepository.create(user_id: user.id, repository_id: repository.id, forked: false)
-  end
+  end # end add_collaborator
 
   def get_repos_by_languages(language)
     found_repos = []
@@ -66,12 +67,32 @@ class User < ActiveRecord::Base
       repo.repository_languages.each do |repo_lang|
         if repo_lang.language_id == language.id
           found_repos << repo
-        end
-      end
-    end
+        end # end if
+      end # end each
+    end # end each
     found_repos
-  end
-end
+  end # end get_repos_by_languages
+
+  def follow_user(username)
+    user = User.find_by(username: username)
+    Follower.create(follower_id: self.id, followee_id: user.id)
+  end  # end follow_user
+
+  def my_followers
+    follows = Follower.where(followee_id: self.id)
+    follows.map do |follow|
+      follow.follower
+    end # end map
+  end # end my_followers
+
+  def users_i_follow
+    my_users = Follower.where(follower_id: self.id)
+    my_users.map do |my_user|
+      my_user.followee
+    end # end map
+  end # end users_i_follow
+
+end # end class User
 
 # ^^^^ test data ^^^^
 # repo = Repository.find(94)
